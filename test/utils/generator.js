64b7a47271602956f8f79b59178b6c0ff54dc21d
Sylvain Foucher <slvnfchr@gmail.com>
2016-10-25 11:53:06 +0200

    Remove static method createIP for simplification purposes

--- test/utils/generator.js
+++ test/utils/generator.js
@@ -5,7 +5,7 @@ module.exports = function generator(input, output) {
 	const length = parseInt(input.length.read().data, 10);
 	const interval = parseInt(input.interval.read().data, 10);
 	const emitter = index => () => {
-		output.out.send(output.out.createIP({ name: index }));
+		output.out.send({ name: index });
 	};
 	for (let i = 0; i < length; i += 1) {
 		setTimeout(emitter(i), i * interval);
